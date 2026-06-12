c6c4cf568e65398e3b6bcd68d948f6ad50aba332
windsor519 <127796845+windsor519@users.noreply.github.com>
2026-06-12 10:58:21 -0400

    Add event category modules

---
+++ src/data/events/career.js
@@ -0,0 +1,4 @@
+// Move career-related event objects into this array as you refactor.
+const careerEvents = [];
+
+export default careerEvents;
---
+++ src/data/events/wallet.js
@@ -0,0 +1,4 @@
+// Move money and wallet-related event objects into this array as you refactor.
+const walletEvents = [];
+
+export default walletEvents;
